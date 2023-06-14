1be79f1e11fdcaf1c8ad85af02f351993b3d3122
farzam <farzamsh1377@gmail.com>
2023-06-14 15:45:57 +0430

    start syncing

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const forecastDiv = document.querySelector('#forecast');
+const searchButton = document.getElementById('search-button');
+const locationInput = document.getElementById('location-input');
+const locationName = document.getElementById('location-name');
+const currentWeatherIcon = document.getElementById('current-weather-icon');
+const currentTemperature = document.getElementById('current-temperature');
+const currentDescription = document.getElementById('current-description');
+const forecastList = document.getElementById('forecast-list');
+const tomorrowWeatherIcon = document.getElementById('tomorrow-weather-icon');
+const tomorrowTemperature = document.getElementById('tomorrow-temperature');
+const tomorrowDescription = document.getElementById('tomorrow-description');
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const city = input.value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`;
-
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const temperature = data.main.temp;
-      const description = data.weather[0].description;
-      const forecast = `The temperature in ${city} is ${temperature}°C with ${description}.`;
-      forecastDiv.textContent = forecast;
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      forecastDiv.textContent = 'An error occurred. Please try again later.';
-    });
+searchButton.addEventListener('click', () => {
+  const location = locationInput.value;
+  fetchWeatherData(location);
 });
+
+function fetchWeatherData(location) {
+  // Make an API request to fetch weather data for the given location
+  // You can use the fetch API or any library of your choice
+  
+  // Once you receive the data, update the HTML elements with the relevant information
+  // For example:
+  locationName.textContent = location;
+  currentTemperature.textContent = '25°C';
+  currentDescription.textContent = 'Sunny';
+
+  // Update tomorrow's forecast
+  tomorrowTemperature.textContent = '27°C';
+  tomorrowDescription.textContent = 'Partly Cloudy';
+
+  // You can dynamically create forecast items based on the received data
+  const forecastData = [
+    { day: 'Monday', temperature: '28°C', description: 'Sunny' },
+    { day: 'Tuesday', temperature: '26°C', description: 'Partly Cloudy' },
+    { day: 'Wednesday', temperature: '24°C', description: 'Rain' },
+    // Add more forecast data as needed
+  ];
+
+  forecastList.innerHTML = '';
+  forecastData.forEach(data => {
+    const forecastItem = document.createElement('div');
+    forecastItem.classList.add('forecast-item');
+    forecastItem.innerHTML = `
+      <h4>${data.day}</h4>
+      <p>${data.temperature}</p>
+      <p>${data.description}</p>
+    `;
+    forecastList.appendChild(forecastItem);
+  });
+}
